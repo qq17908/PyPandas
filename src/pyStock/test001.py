956c69e4ef0be4816f00617f5c6644d5627aac64
qq17908 <vippopo@163.com>
2016-03-28 12:48:18 +0800

    解决中文乱码问题！ test001增加：_fromUtf8

--- src/pyStock/test001.py
+++ src/pyStock/test001.py
@@ -3,20 +3,33 @@
 from PyQt4 import QtCore, QtGui
 from PyQt4.QtGui import QMessageBox
 from PyQt4.QtGui import QWidget
-  
+
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+
 class MyWindow(QtGui.QWidget):
     def __init__(self):
         super(MyWindow,self).__init__()
         self.myButton = QtGui.QPushButton(self)
-        self.myButton.setObjectName("myButton")
-        self.myButton.setText("Test")
+        self.myButton.setObjectName(_fromUtf8("myButton"))
+        self.myButton.setText(_fromUtf8("点击"))
         self.myButton.clicked.connect(self.msg)
 
     def msg(self):
-        reply = QMessageBox.information(self,                         #使用infomation信息框
-                                    "标题",
-                                    "消息",
-                                    QMessageBox.Yes | QMessageBox.No)
+        reply = QMessageBox.information(self,_fromUtf8("标题"),_fromUtf8("消息"),QMessageBox.Yes | QMessageBox.No)
 
 if __name__=="__main__":  
     import sys  
