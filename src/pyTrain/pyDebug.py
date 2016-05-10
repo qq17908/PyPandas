2666d94e6567db09736b28d833ca55e416ff6a58
qq17908 <vippopo@163.com>
2016-05-10 22:33:02 +0800

    add pyTrain!

---
+++ src/pyTrain/pyDebug.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#########################################################
+#1、错误处理  try...except...finally...
+#2、调试
+#3、单元测试
+#4、文档测试
+#########################################################
+
+
+#断言
+#assert 如果失败则跑错AssertionError
+def foo(s):
+    n = int(s)
+    assert n != 0 ,'n is zero!'
+    return 10 / n
+
+def main():
+    foo('0')
+
+main()
+
+
+#logging
+#logging不会抛出错误，但会输出文件
+#debug，info，warning，error等几个级别
+#level=WARNING后debug和info就不起作用
+#level=INFO后logging.debug就不起作用。
+
+import logging
+logging.basicConfig(level=logging.INFO)
+
+s = '0'
+n = int(s)
+logging.info('n=%d' % n)
+print(10/n)
+
+#PDB
+#-m pdb err.py  执行后进入调试模式
+#l - 查看代码
+#n - 单步调试
+#p - p 变量名  查看变量
+#q - 结束调试
--- src/qtTrain/stockdisplay.py
+++ src/qtTrain/stockdisplay.py
@@ -1,3 +1,5 @@
+#coding:utf-8
+
 import sys
 from PyQt4 import QtCore, QtGui, uic
 
@@ -10,6 +12,13 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
         QtGui.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
+     
+    def butClicked(self):
+        sender = self.sender()
+        self.statusBar().showMessage(sender.text() + 'ok!')
+    
+    def showDialog(self):
+        text, ok = QtGui.QInputDialog.getText(self, '输入', '输入姓名')
     
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
