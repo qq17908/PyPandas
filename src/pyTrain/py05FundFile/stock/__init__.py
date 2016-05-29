f79665fb36855a4c5033d68ffac77656e5209c79
qq17908 <vippopo@163.com>
2016-05-29 21:23:35 +0800

    no message

---
+++ src/pyTrain/py05FundFile/stock/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 29 21:13:09 2016
+
+@author: Paul
+"""
+
---
+++ src/pyTrain/py05FundFile/stock/stockbasics.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 29 21:14:40 2016
+
+@author: Paul
+"""
+    
+"""
+    get_stock_basics()
+    获取沪深上市公司基本情况
+    Return
+    --------
+    DataFrame
+               code,代码
+               name,名称
+               industry,细分行业
+               area,地区
+               pe,市盈率
+               outstanding,流通股本
+               totals,总股本(万)
+               totalAssets,总资产(万)
+               liquidAssets,流动资产
+               fixedAssets,固定资产
+               reserved,公积金
+               reservedPerShare,每股公积金
+               eps,每股收益
+               bvps,每股净资
+               pb,市净率
+               timeToMarket,上市日期
+"""
+
